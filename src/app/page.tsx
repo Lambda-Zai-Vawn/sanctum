a46f126fcb0c1607c541cd5cf6fe0c3ec2c10359
Bryan Marlow <brylow@aevon.cloud>
2025-08-11 07:26:31 +0000

    continue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,10 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="relative min-h-screen w-full overflow-x-hidden bg-background">
-        <div className="absolute top-0 left-0 -z-10 h-full w-full">
-            <div className="absolute inset-0 -z-10 h-full w-full bg-aurora [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
-        </div>
-
+        {/* The main layout aurora is not used here, the 3D scene has its own background */}
         <div className="relative z-0 flex min-h-screen flex-col">
             <Navigation />
-            <main className="flex-grow">
+            <main className="flex-grow contents">
                 <HomeContent />
             </main>
             <Footer />
